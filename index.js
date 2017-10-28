6953b53a8ff3cf98ec10d842bcaaeb3b77bd7c64
LMokrane <mokrane.lateb@sqy.fr>
2017-10-28 23:19:20 +0200

    multiplication

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 class Matrice {
   constructor() {
   }
-  multiplication(m1, m2) {
-    
+  multiplication(A, B) {
+    let n= A.length, m= B.length, p= B[0].length;
+    let C = [];
+    for (let i=0; i<n; i++) {
+      C[i] = [];
+      for (let j=0; j<p; j++) {
+        C[i][j] = 0;
+        for (let k=0; k<m; k++) {
+          C[i][j] += A[i][k] * B[k][j];
+        }
+      }
+    }
+    return C;
   }
 }
 
